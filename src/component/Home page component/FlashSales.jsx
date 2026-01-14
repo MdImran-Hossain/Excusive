fcdb65b33258fd0e89eb6926610ff2fc0d6e5717
MdImran-Hossain <mdimranhossain.web@gmail.com>
2026-01-14 22:00:26 +0600

    flash section complete

--- src/component/Home page component/FlashSales.jsx
+++ src/component/Home page component/FlashSales.jsx
@@ -1,32 +1,71 @@
-import React from 'react'
-import Heading from '../commonComponent/Heading'
-import Time from '../commonComponent/Time'
-import ProductCard from '../commonComponent/ProductCard'
+import React from "react";
+import Heading from "../commonComponent/Heading";
+import Time from "../commonComponent/Time";
+import ProductCard from "../commonComponent/ProductCard";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination } from "swiper/modules";
+import "swiper/css/navigation";
+
+import "swiper/css";
+import "swiper/css/pagination";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 
 const FlashSales = () => {
   return (
-    <>
-    <section className='pt-35 pb-12'>
-      <div className=' container m-auto'>
-        <div>
-          <div className='flex flex-col gap-10'>
-            <div className='flex justify-start items-end gap-[87px]'>
-              <Heading  title={'Flash Sales'} subtitle={"Today’s"}/>
-              <div>
-                <Time />
-              </div>
-            </div>
-            <div>
-              <ProductCard />
+    <section className="pt-35 pb-12">
+      <div className="container m-auto">
+        <div className="flex flex-col gap-10">
+          <div className=" flex justify-between items-end">
+            <div className="flex justify-start items-end gap-[87px]">
+            <Heading title="Flash Sales" subtitle="Today’s" />
+            <Time />
+          </div>
+          <div className="flex justify-center items-center gap-2">
+            <div className=" prev w-[46px] h-[46px] bg-f5f5f5 rounded-full flex justify-center items-center"> <span><FaArrowLeft /></span></div>
+            <div className="next w-[46px] h-[46px] bg-f5f5f5 rounded-full flex justify-center items-center"> <span><FaArrowRight /></span></div>
+          </div>
+          </div>
 
-            </div>
-            
+          <div className=" overflow-hidden">
+            <Swiper
+            loop={true}
+              navigation={{
+    prevEl: ".prev",
+    nextEl: ".next",
+  }}
+            modules={[ Navigation]}
+            spaceBetween={20}
+            breakpoints={{
+              0: {
+                slidesPerView: 1,
+              },
+              480: {
+                slidesPerView: 2,
+              },
+              640: {
+                slidesPerView: 3,
+              },
+              768: {
+                slidesPerView: 4,
+              },
+              1024: {
+                slidesPerView: 6,
+              },
+            }}
+            className="mySwiper"
+          >
+            {[...Array(10)].map((_, index) => (
+              <SwiperSlide key={index} className="!w-[270px]">
+                <ProductCard />
+              </SwiperSlide>
+            ))}
+          </Swiper>
           </div>
         </div>
       </div>
     </section>
-    </>
-  )
-}
+  );
+};
 
-export default FlashSales
+export default FlashSales;
--- src/component/commonComponent/ProductCard.jsx
+++ src/component/commonComponent/ProductCard.jsx
@@ -1,42 +1,60 @@
-import React from 'react'
-import productImages from "../../assets/productImage/p1.png"
-import { FaRegHeart, FaStar } from 'react-icons/fa'
-import { LuEye } from 'react-icons/lu'
+import React from "react";
+import productImages from "../../assets/productImage/p1.png";
+import { FaRegHeart, FaStar } from "react-icons/fa";
+import { LuEye } from "react-icons/lu";
 
 const ProductCard = () => {
   return (
     <>
-        <div className=' max-w-[270px] flex flex-col gap-4'>
-            <div className='py-[35px] px-10 bg-f5f5f5 rounded flex justify-center items-center relative'>
-                <img src={productImages} alt={productImages} />
-                <div className='flex flex-col items-start gap-2 absolute right-3 top-3'>
-                    <div className='w-[34px] h-[34px] rounded-full bg-white flex justify-center items-center '>
-                        <FaRegHeart />
-                    </div>
-                    <div className='w-[34px] h-[34px] rounded-full bg-white flex justify-center items-center '>
-                       <LuEye />
-                    </div>
-                </div>
-                <div className='py-1 px-3 bg-red rounded text-[12px] text-f5f5f5 font-poppins font-normal absolute top-3 left-3'><p>-40%</p></div>
+      <div className=" max-w-[270px] flex flex-col gap-4 addCart_wraaper transition-all cursor-pointer">
+        <div className="pt-[35px] pb-11 px-10 bg-f5f5f5 rounded flex justify-center items-center relative  overflow-hidden">
+          <img src={productImages} alt={productImages} />
+          <div className="flex flex-col items-start gap-2 absolute right-3 top-3">
+            <div className="w-[34px] h-[34px] rounded-full bg-white flex justify-center items-center ">
+              <FaRegHeart />
             </div>
-            <div className='flex flex-col gap-2'>
-                <h3 className='font-poppins font-semibold text-[16px] text-black'>HAVIT HV-G92 Gamepad</h3>
-                <p className='flex gap-3'>
-                    <span className='font-poppins font-medium text-[16px] text-red'> $120</span>
-                    <span className='font-poppins font-medium text-[16px] text-black opacity-50 text line-through'>$160</span>
-                </p>
-                <div className='flex items-center'>
-                    <span className='text-[#FFAD33] w-5 h-5'><FaStar /></span>
-                    <span className='text-[#FFAD33] w-5 h-5'><FaStar /></span>
-                    <span className='text-[#FFAD33] w-5 h-5'><FaStar /></span>
-                    <span className='text-[#FFAD33] w-5 h-5'><FaStar /></span>
-                    <span className='text-[#FFAD33] w-5 h-5'><FaStar /></span>
-                    <span className='font-poppins font-medium text-[14px] text-black opacity-50 pl-1'>(88)</span>
-                </div>
+            <div className="w-[34px] h-[34px] rounded-full bg-white flex justify-center items-center ">
+              <LuEye />
             </div>
-        </div>
-    </>
-  )
-}
+          </div>
+          <div className="py-1 px-3 bg-red rounded text-[12px] text-f5f5f5 font-poppins font-normal absolute top-3 left-3">
+            <p>-40%</p>
+          </div>
+            <div className="flex justify-center items-center py-2 bg-black text-[16px] text-white font-medium font-poppins absolute bottom-[-100%] left-0 right-0 addCart transition-all">
+                <p>Add To Cart</p>
+            </div>
 
-export default ProductCard
+        </div>
+        <div className="flex flex-col gap-2">
+          <h3 className="font-poppins font-semibold text-[16px] text-black">
+            HAVIT HV-G92 Gamepad
+          </h3>
+          <p className="flex gap-3">
+            <span className="font-poppins font-medium text-[16px] text-red">
+              {" "}
+              $120
+            </span>
+            <span className="font-poppins font-medium text-[16px] text-black opacity-50 text line-through">
+              $160
+            </span>
+          </p>
+
+          <div className="flex items-center">
+            {[...new Array(5)].map((_, Index) => {
+              return(
+                <span key={Index} className="text-[#FFAD33] w-5 h-5">
+                <FaStar />
+              </span>
+              )
+            })}
+            <span className="font-poppins font-medium text-[14px] text-black opacity-50 pl-1">
+              (88)
+            </span>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ProductCard;
